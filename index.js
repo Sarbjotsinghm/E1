286f0f6eefd7f011a0edf4ccb19376e3065b1b98
Sarbjot Singh <sarbjots341@gmail.com>
2023-03-08 02:54:00 -0600

    completed first video

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as TargetSum} from './app.json';
+
+// AppRegistry.registerComponent(TargetSum, () => App);
+
+import { AppRegistry } from 'react-native';
+
+import App from './src/components/App';
+
+AppRegistry.registerComponent('TargetSum', () => App);
