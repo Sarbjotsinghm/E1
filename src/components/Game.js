954a35ae26ee01dc6dc4be2e889abfd96622886c
Sarbjot Singh <sarbjots341@gmail.com>
2023-03-08 11:47:12 -0600

    got some functional errors in game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,63 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { View, Text, StyleSheet } from 'react-native'
+import {View, Text, StyleSheet} from 'react-native';
 
 class Game extends React.Component {
-    static propTypes = {
+  static propTypes = {
+    RandomNumberCount: PropTypes.number.isRequired,
+  };
 
-        randomNumberCount: PropTypes.string.isRequired,
-    };
+  randomNumbers = Array.from({lenght: this.props.RandomNumberCount}).map(
+    () => 1 + Math.floor(10 * Math.random()),
+  );
+  target = this.randomNumbers
+    .slice(0, this.props.RandomNumberCount - 2)
+    .reduce((acc, curr) => acc + curr, 0);
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.target}>{this.target}</Text>
 
-    randomNumbers = Array
-        .from({ lenght: this.props.randomNumberCount })
-        .map(() => 1 + Math.floor(10 * Math.random()));
-    target = this.randomNumbers
-        .slice(0, this.props.randomNumberCount - 2)
-        .reduce((acc, curr) => acc + curr, 0);
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.target}>{this.target}</Text>
-                <View style={styles.randomContainer}>
-                {this.randomNumbers.map((randomNumber, index) =>
-                    <Text key={index}>{randomNumber}</Text>)}
-                     </View>
-             
-            </View>
-        );
-    }
+        {this.randomNumbers.map((randomNumber, index) => (
+          <Text key={index}>{this.target}</Text>
+        ))}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#ddd',
-        flex: 1,
-        paddingTop: 30,
-    },
+  container: {
+    backgroundColor: '#ddd',
+    flex: 1,
+    paddingTop: 30,
+  },
 
-    target: {
-        fontSize: 40,
-         backgroundColor: '#aaa',
-        margin: 50,
-        textAlign: 'center',
-    },
+  target: {
+    fontSize: 40,
+    backgroundColor: '#aaa',
+    margin: 50,
+    textAlign: 'center',
+  },
 
-    randomContainer:{
-        flex: 1,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent:'space-around',
-    },
+  randomContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
 
-    random: {
-bacckgroundColor: '#999',
-width: 100,
-morginHorizontal: 15,
-marginVertical: 25,
-fontSize: 35,
-textAlign: 'center',
-    }
+  random: {
+    bacckgroundColor: '#999',
+    width: 100,
+    morginHorizontal: 15,
+    marginVertical: 25,
+    fontSize: 35,
+    textAlign: 'center',
+  },
 });
 
 export default Game;
