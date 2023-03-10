c92ad01e09a87a8946d45d802579fd19d599841e
Sarbjot Singh <sarbjots341@gmail.com>
2023-03-10 12:53:58 -0600

    here i have added handlepress and react component same as game component and add Touchable opacity in it.

--- src/components/RandomNumber.js
+++ src/components/RandomNumber.js
@@ -1,3 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Text, TouchableOpacity, StyleSheet} from 'react-native';
+
+class RandomNumber extends React.Component {
+  static propTypes = {
+    id: PropTypes.number.isRequired,
+    number: PropTypes.number.isRequired,
+    isDisabled: PropTypes.bool.isRequired,
+    onPress: PropTypes.func.isRequired,
+  };
+  handlePress = () => {
+    if (this.props.isDisabled) {
+      return;
+    }
+    this.props.onPress(this.props.id);
+  };
+  render() {
+    return (
+      <TouchableOpacity onPress={this.handlePress}>
+        <Text style={[styles.random, this.props.isDisabled && styles.disabled]}>
+          {' '}
+          {this.props.number}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+}
