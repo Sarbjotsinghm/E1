65af55e50cb15d0dcca0c68b3d1d879e6ad775ca
Sarbjot Singh <sarbjots341@gmail.com>
2023-03-10 12:46:38 -0600

    in game component comment out the code and start over step by step.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,61 +1,181 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+// import {View, Text, StyleSheet} from 'react-native';
+
+// class Game extends React.Component {
+//   static propTypes = {
+//     randomNumberCount: PropTypes.number.isRequired,
+//   };
+//   randomNumbers = Array.from({lenght: this.props.randomNumberCount}).map(
+//     () => 1 + Math.floor(10 * Math.random()),
+//   );
+//   target = this.randomNumbers
+//     .slice(0, this.props.randomNumberCount - 2)
+//     .reduce((acc, curr) => acc + curr, 0);
+//   // todo: shuffle random numbers
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.target}>{this.target}</Text>
+//         <View style={styles.randomContainer}>
+//           {this.randomNumbers.map((randomNumber, index) =>
+//             <Text style={styles.random} key={index}>
+//               {randomNumbers}
+//             </Text>
+//           ))}
+//         </View>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     backgroundColor: '#ddd',
+//     flex: 1,
+//     paddingTop: 30,
+//   },
+
+//   target: {
+//     fontSize: 40,
+//     backgroundColor: '#aaa',
+//     margin: 50,
+//     textAlign: 'center',
+//   },
+
+//   randomContainer: {
+//     flex: 1,
+//     flexDirection: 'row',
+//     flexWrap: 'wrap',
+//     justifyContent: 'space-around',
+//   },
+
+//   random: {
+//     bacckgroundColor: '#999',
+//     width: 100,
+//     morginHorizontal: 15,
+//     marginVertical: 25,
+//     fontSize: 35,
+//     textAlign: 'center',
+//   },
+// });
+
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, Button, StyleSheet} from 'react-native';
+
+import RandomNumber from './RandomNumber';
+import shuffle from 'lodash.shuffle';
 
 class Game extends React.Component {
   static propTypes = {
-    RandomNumberCount: PropTypes.number.isRequired,
+    randomNumberCount: PropTypes.number.isRequired,
+    initialSeconds: PropTypes.number.isRequired,
+    onPlayAgain: PropTypes.func.isRequired,
   };
-
-  randomNumbers = Array.from({lenght: this.props.RandomNumberCount}).map(
+  state = {
+    selectedIds: [],
+    remainingSeconds: this.props.initialSeconds,
+  };
+  gameStatus = 'PLAYING';
+  randomNumbers = Array.from({length: this.props.randomNumberCount}).map(
     () => 1 + Math.floor(10 * Math.random()),
   );
   target = this.randomNumbers
-    .slice(0, this.props.RandomNumberCount - 2)
+    .slice(0, this.props.randomNumberCount - 2)
     .reduce((acc, curr) => acc + curr, 0);
+
+  shuffledRandomNumbers = shuffle(this.randomNumbers);
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState(
+        prevState => {
+          return {remainingSeconds: prevState.remainingSeconds - 1};
+        },
+        () => {
+          if (this.state.remainingSeconds === 0) {
+            clearInterval(this.intervalId);
+          }
+        },
+      );
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  isNumberSelected = numberIndex => {
+    return this.state.selectedIds.indexOf(numberIndex) >= 0;
+  };
+
+  selectNumber = numberIndex => {
+    this.setState(prevState => ({
+      selectedIds: [...prevState.selectedIds, numberIndex],
+    }));
+  };
+
+  componentWillUpdate(nextProps, nextState) {
+    if (
+      nextState.selectedIds !== this.state.selectedIds ||
+      nextState.remainingSeconds === 0
+    ) {
+      this.gameStatus = this.calcGameStatus(nextState);
+      if (this.gameStatus !== 'PLAYING') {
+        clearInterval(this.intervalId);
+      }
+    }
+  }
+
+  calcGameStatus = nextState => {
+    const sumSelected = nextState.selectedIds.reduce((acc, curr) => {
+      return acc + this.shuffledRandomNumbers[curr];
+    }, 0);
+    if (nextState.remainingSeconds === 0) {
+      return 'LOST';
+    }
+    if (sumSelected < this.target) {
+      return 'PLAYING';
+    }
+    if (sumSelected === this.target) {
+      return 'WON';
+    }
+    if (sumSelected > this.target) {
+      return 'LOST';
+    }
+    // console.log(sumSelected);
+  };
+
   render() {
+    const gameStatus = this.gameStatus;
     return (
       <View style={styles.container}>
-        <Text style={styles.target}>{this.target}</Text>
-
-        {this.randomNumbers.map((randomNumber, index) => (
-          <Text key={index}>{this.target}</Text>
-        ))}
+        <Text style={[styles.target, styles[`STATUS_${gameStatus}`]]}>
+          {this.target}
+        </Text>
+        <View style={styles.randomContainer}>
+          {this.shuffledRandomNumbers.map((randomNumber, index) => (
+            <RandomNumber
+              key={index}
+              id={index}
+              number={randomNumber}
+              isDisabled={
+                this.isNumberSelected(index) || gameStatus !== 'PLAYING'
+              }
+              onPress={this.selectNumber}
+            />
+          ))}
+        </View>
+        {this.gameStatus !== 'PLAYING' && (
+          <Button title="Play Again" onPress={this.props.onPlayAgain} />
+        )}
+        <Text>{this.state.remainingSeconds}</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#ddd',
-    flex: 1,
-    paddingTop: 30,
-  },
-
-  target: {
-    fontSize: 40,
-    backgroundColor: '#aaa',
-    margin: 50,
-    textAlign: 'center',
-  },
-
-  randomContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-  },
-
-  random: {
-    bacckgroundColor: '#999',
-    width: 100,
-    morginHorizontal: 15,
-    marginVertical: 25,
-    fontSize: 35,
-    textAlign: 'center',
-  },
-});
 
 export default Game;
